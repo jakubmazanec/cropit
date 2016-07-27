b51ebfdf92c626bd82a201f98a113387c9616689
unknown <jakubmazanec@DAVIDBERAN01.q2cz.local>
2016-07-27 16:12:10 +0200

    Build dist version

--- dist/jquery.cropit.js
+++ dist/jquery.cropit.js
@@ -920,6 +920,8 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	      if (minZoom === 'fit') {
 	        this.minZoom = Math.min(widthRatio, heightRatio);
+	      } else if (typeof minZoom === 'number') {
+	        this.minZoom = Math.max(minZoom, 0.1);
 	      } else {
 	        this.minZoom = Math.max(widthRatio, heightRatio);
 	      }
